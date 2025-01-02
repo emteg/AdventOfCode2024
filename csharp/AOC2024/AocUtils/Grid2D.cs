1ba9acf6509dc1936255072e04d3dcee8bb9ce95
Matthias Grabowski <matthias.grabowski@theobald-software.com>
2025-01-02 11:23:08 +0100

    Added utility project and classes

---
+++ csharp/AOC2024/AocUtils/Grid2D.cs
@@ -0,0 +1,84 @@
+﻿namespace AocUtils;
+
+public class Grid2D<T>
+{
+    public List<List<GridCell2D<T>>> Cells { get; } = [];
+    public uint Height => (uint)Cells.Count;
+    public uint Width => Cells.Count > 0 ? (uint)Cells[0].Count : 0;
+
+    public bool HasCellAt(uint x, uint y)
+    {
+        try
+        {
+            _ = Cells[(int)y][(int)x];
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public bool HasCellAt(int x, int y)
+    {
+        try
+        {
+            _ = Cells[y][x];
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    
+    public bool HasCellAt(long x, long y)
+    {
+        if (x < int.MinValue || x > int.MaxValue || y < int.MinValue || y > int.MaxValue)
+            throw new ArgumentOutOfRangeException();
+        
+        return HasCellAt((int)x, (int)y);
+    }
+    
+    public GridCell2D<T> Get(uint x, uint y)
+    {
+        if (!HasCellAt(x, y))
+            throw new InvalidOperationException($"No cell found at location '{x}'/'{y}'");
+        
+        return Cells[(int)y][(int)x];
+    }
+    
+    public GridCell2D<T> Get(int x, int y)
+    {
+        if (!HasCellAt(x, y))
+            throw new InvalidOperationException($"No cell found at location '{x}'/'{y}'");
+        
+        return Cells[y][x];
+    }
+    
+    public GridCell2D<T> Get(long x, long y)
+    {
+        if (!HasCellAt(x, y))
+            throw new InvalidOperationException($"No cell found at location '{x}'/'{y}'");
+        
+        return Cells[(int)y][(int)x];
+
+    }
+
+    public List<GridCell2D<T>> AddRow()
+    {
+        List<GridCell2D<T>> row = [];
+        Cells.Add(row);
+        return row;
+    }
+
+    public uint Count(Func<GridCell2D<T>, bool> predicate)
+    {
+        uint result = 0;
+        foreach (List<GridCell2D<T>> row in Cells)
+        {
+            result += (uint)row.Count(predicate);
+        }
+        return result;
+    }
+}
---
+++ csharp/AOC2024/AocUtils/GridCell2D.cs
@@ -0,0 +1,43 @@
+﻿namespace AocUtils;
+
+/// <summary>
+/// A cell in a 2D grid with references to its grid neighbors.
+/// </summary>
+public class GridCell2D<T>
+{
+    public char Char { get; set; }
+    public T? Payload { get; set; }
+    public uint X { get; }
+    public uint Y { get; }
+    public GridCell2D<T>? RightNeighbor { get; protected set; }
+    public GridCell2D<T>? LeftNeighbor { get; protected set; }
+    public GridCell2D<T>? UpNeighbor { get; protected set; }
+    public GridCell2D<T>? DownNeighbor { get; protected set; }
+
+    public GridCell2D(uint x, uint y, char c, T? payload = default)
+    {
+        Char = c;
+        X = x;
+        Y = y;
+        Payload = payload;
+    }
+
+    public GridCell2D<T> ConnectLeftNeighbor(GridCell2D<T> leftNeighbor)
+    {
+        LeftNeighbor = leftNeighbor;
+        leftNeighbor.RightNeighbor = this;
+        return this;
+    }
+
+    public GridCell2D<T> ConnectUpNeighbor(GridCell2D<T> upNeighbor)
+    {
+        UpNeighbor = upNeighbor;
+        upNeighbor.DownNeighbor = this;
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return Char.ToString();
+    }
+}
